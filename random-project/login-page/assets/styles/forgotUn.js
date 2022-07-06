92258cf7b0eedb4ba4315658ecc0bbea0495fb06
tafifa <tafifa09@gmail.com>
2022-07-06 15:34:46 +0700

    update name folder

---
+++ random-project/login-page/assets/styles/forgotUn.js
@@ -0,0 +1,10 @@
+const uname = document.getElementById('username');
+const pword = document.getElementById('password');
+function forgotUn {
+    uname.style.display = "block";
+    pword.style.display = "none";
+}
+function forgotUn {
+    uname.style.display = "none";
+    pword.style.display = "block";
+}
